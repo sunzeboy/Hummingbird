ba018c6040aeb9ebfbd5aaf4c9415648f88efd30
sunzeboy <13162153278@163.com>
2017-03-25 16:20:06 +0800

    添加HummingbirdKit

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "Hummingbird"
+    name: "Hummingbird",
+    targets: [
+        Target(name: "HummingbirdKit", dependencies: []),
+        Target(name: "Hummingbird", dependencies: ["HummingbirdKit"])
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"),
+        .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 8)
+    ],
+    exclude: [ "Tests/Fixtures" ]
 )
